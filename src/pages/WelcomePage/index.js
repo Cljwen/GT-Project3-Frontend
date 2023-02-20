8c860a1b887b1f35eee8f9618b77ee13ef91b797
Cljwen <carmenlaujwn@gmail.com>
2023-02-20 22:45:44 +0800

    made minor changes

--- src/pages/WelcomePage/index.js
+++ src/pages/WelcomePage/index.js
@@ -69,6 +69,18 @@ export default function WelcomePage() {
   }, [user, accessToken]);
   console.log(accessToken);
 
+  const handleSignUp = () => {
+    const redirectUri = "http://localhost:3001/*/homepage";
+    const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+    const queryParams = {
+      client_id: clientId,
+      redirect_uri: redirectUri,
+      // response_type: 'code',
+      scope: "openid profile email",
+      screen_hint: "signup",
+    };
+  };
+
   return (
     <Space
       direction="vertical"
@@ -97,8 +109,16 @@ export default function WelcomePage() {
               <Button
                 className="button"
                 type="primary"
-                style={{ backgroundColor: '#ff7e55', color: 'white' }}
-                onClick={loginWithRedirect}>
+                style={{ backgroundColor: "#ff7e55", color: "white" }}
+                onClick={handleSignUp}
+              >
+                Sign Up
+              </Button>
+              <Button
+                type="primary"
+                style={{ backgroundColor: "#ff7e55", color: "white" }}
+                onClick={loginWithRedirect}
+              >
                 Sign Up / Login
               </Button>
             </Space>
@@ -116,9 +136,10 @@ export default function WelcomePage() {
           <div className="press">
             <h2>These are some listings you might be interested in:</h2>
 
-            <Row gutter={16}>
-              {listingsReturned.map(({ category, item_name, photo_url }) => (
-                <Col span={6}>
+          <Row gutter={16}>
+            {listingsReturned.map(
+              ({ category, item_name, photo_url }, index) => (
+                <Col span={6} key={index}>
                   <Card
                     hoverable
                     style={{ width: 300, margin: 20 }}
