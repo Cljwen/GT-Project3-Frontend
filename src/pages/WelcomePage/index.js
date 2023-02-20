af75543e47abb22d030fc27d6f38a7fc6773b6b8
oheirelavv <oheirelav@gmail.com>
2023-02-20 22:28:28 +0800

    edited welcome page links

--- src/pages/WelcomePage/index.js
+++ src/pages/WelcomePage/index.js
@@ -1,16 +1,9 @@
 // welcome page with short intro, login and sign up buttons
-
 import React from 'react';
-import { Layout, Space, Menu, Button, Card, Col, Row, Avatar, Carousel } from 'antd';
+import { Layout, Space, Menu, Button, Card, Col, Row, Avatar } from 'antd';
 import intropage from '../../assets/images/intropage.jpg';
 import logo from '../../assets/images/logo.png';
 import banner from '../../assets/images/banner.png';
-import rocket from '../../assets/images/rocket.png';
-import cloudinary from '../../assets/images/cloudinary.png';
-import postgres from '../../assets/images/postgres.png';
-import sendgrid from '../../assets/images/sendgrid.png';
-import react from '../../assets/images/react.webp';
-import antdesign from '../../assets/images/ant-design.png';
 
 import './welcomepage.css';
 import { MessageOutlined, LikeOutlined } from '@ant-design/icons';
@@ -35,8 +28,7 @@ const contentStyle = {
   minHeight: 400,
   lineHeight: '120px',
   color: '#303841',
-  backgroundColor: '#eeeeee',
-  paddingTop: '50px'
+  backgroundColor: '#eeeeee'
 };
 const footerStyle = {
   textAlign: 'center',
@@ -77,37 +69,27 @@ export default function WelcomePage() {
       }}
       size={[0, 48]}>
       <Layout>
-        <Header className="header" style={headerStyle}>
+        <Header style={headerStyle}>
           <Menu
-            className="navbar"
             style={{ backgroundColor: '#eeeeee', color: '#ff7e55' }}
             mode="horizontal"
             defaultSelectedKeys={['0']}>
             <img height={60} src={logo} alt="logo" />
-            <Menu.Item key="0">
-              <a href="#about">How it works</a>
-            </Menu.Item>
-            <Menu.Item key="1">
-              <a href="#press">Press</a>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <a href="#contact">Contact Us</a>
-            </Menu.Item>
+            <Menu.Item key="0">About</Menu.Item>
+            <Menu.Item key="1">Press</Menu.Item>
+            <Menu.Item key="2">Contact Us</Menu.Item>
             <Space wrap>
               <Button
-                className="button"
                 type="primary"
-                style={{ backgroundColor: "#ff7e55", color: "white" }}
-                onClick={handleSignUp}
-              >
-                Sign Up
+                style={{ backgroundColor: '#ff7e55', color: 'white' }}
+                onClick={loginWithRedirect}>
+                Sign Up / Login
               </Button>
               <Button
                 type="primary"
-                style={{ backgroundColor: "#ff7e55", color: "white" }}
-                onClick={loginWithRedirect}
-              >
-                Sign Up / Login
+                style={{ backgroundColor: '#ff7e55', color: 'white' }}
+                onClick={logout}>
+                Logout
               </Button>
             </Space>
           </Menu>
@@ -121,77 +103,33 @@ export default function WelcomePage() {
               <p>Your local app for giving and requesting things.</p>
             </div>
           </div>
-          <div className="press">
-            <h2>These are some listings you might be interested in:</h2>
+          <h2>Here's how it works: </h2>
+          <img className="banner" src={banner} alt="banner" />
+          <h2>These are some listings you might be interested in:</h2>
 
           <Row gutter={16}>
-            {listingsReturned.map(
-              ({ category, item_name, photo_url }, index) => (
-                <Col span={6} key={index}>
-                  <Card
-                    hoverable
-                    style={{ width: 300, margin: 20 }}
-                    cover={
-                      <img
-                        alt=""
-                        src={photo_url}
-                        style={{ width: 300, height: 300, objectFit: 'contain' }}
-                      />
-                    }
-                    actions={[<MessageOutlined key="message" />, <LikeOutlined key="like" />]}>
-                    <Meta title={item_name} description={category} />
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-
-            <Space wrap>
-              <Button
-                className="button"
-                type="primary"
-                style={{ backgroundColor: '#ff7e55', color: 'white' }}
-                onClick={loginWithRedirect}>
-                Sign Up / Login
-              </Button>
-            </Space>
-          </div>
-          <div id="about">
-            <h2>Here's how it works: </h2>
-            <img className="banner" src={banner} alt="banner" />
-          </div>
-          <div id="press" className="press">
-            <h2>We are supported by:</h2>
-            <Carousel
-              slidesToShow={3}
-              style={{ margin: '20px 50px' }}
-              autoplay
-              autoplaySpeed={2000}
-              speed={500}>
-              <div className="slide">
-                <img src={rocket} alt="rocket" />
-              </div>
-              <div className="slide">
-                <img src={react} alt="react" />
-              </div>
-              <div className="slide">
-                <img src={cloudinary} alt="cloudinary" />
-              </div>
-              <div className="slide">
-                <img src={postgres} alt="postgres" />
-              </div>
-              <div className="slide">
-                <img src={sendgrid} alt="sendgrid" />
-              </div>
-              <div className="slide">
-                <img src={antdesign} alt="antdesign" />
-              </div>
-            </Carousel>
-          </div>
-          <div className="contact" id="contact">
-            <h2>Contact us</h2>
-            <p>Have any questions? Get in touch with us!</p>
-            <p className="email">hello@giveandtake.sg</p>
-          </div>
+            {listingsReturned.map(({ category, item_name, photo_url }, index) => (
+              <Col span={6} key={index}>
+                <Card
+                  hoverable
+                  style={{ width: 300, margin: 20 }}
+                  cover={
+                    <img
+                      alt=""
+                      src={photo_url}
+                      style={{
+                        width: 300,
+                        height: 300,
+                        objectFit: 'contain'
+                      }}
+                    />
+                  }
+                  actions={[<MessageOutlined key="message" />, <LikeOutlined key="like" />]}>
+                  <Meta title={item_name} description={category} />
+                </Card>
+              </Col>
+            ))}
+          </Row>
         </Content>
         <Footer style={footerStyle}>Copyright © Give & Take 2023 </Footer>
       </Layout>
