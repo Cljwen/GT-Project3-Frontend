4f7797aaf5dbdb5cf88d931bbc1600370b4d704d
oheirelavv <oheirelav@gmail.com>
2023-02-21 22:59:46 +0800

    updated welcome page

--- src/pages/WelcomePage/index.js
+++ src/pages/WelcomePage/index.js
@@ -69,23 +69,33 @@ export default function WelcomePage() {
       }}
       size={[0, 48]}>
       <Layout>
-        <Header style={headerStyle}>
+        <Header className="header" style={headerStyle}>
           <Menu
+            className="navbar"
             style={{ backgroundColor: '#eeeeee', color: '#ff7e55' }}
             mode="horizontal"
             defaultSelectedKeys={['0']}>
             <img height={60} src={logo} alt="logo" />
-            <Menu.Item key="0">About</Menu.Item>
-            <Menu.Item key="1">Press</Menu.Item>
-            <Menu.Item key="2">Contact Us</Menu.Item>
+
+            <Menu.Item key="0">
+              <a href="#about">How it works</a>
+            </Menu.Item>
+            <Menu.Item key="1">
+              <a href="#press">Press</a>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <a href="#contact">Contact Us</a>
+            </Menu.Item>
             <Space wrap>
               <Button
+                className="button"
                 type="primary"
                 style={{ backgroundColor: '#ff7e55', color: 'white' }}
                 onClick={loginWithRedirect}>
                 Sign Up / Login
               </Button>
               <Button
+                className="button"
                 type="primary"
                 style={{ backgroundColor: '#ff7e55', color: 'white' }}
                 onClick={logout}>
@@ -103,33 +113,74 @@ export default function WelcomePage() {
               <p>Your local app for giving and requesting things.</p>
             </div>
           </div>
-          <h2>Here's how it works: </h2>
-          <img className="banner" src={banner} alt="banner" />
-          <h2>These are some listings you might be interested in:</h2>
+          <div className="press">
+            <h2>These are some listings you might be interested in:</h2>
 
           <Row gutter={16}>
-            {listingsReturned.map(({ category, item_name, photo_url }, index) => (
-              <Col span={6} key={index}>
-                <Card
-                  hoverable
-                  style={{ width: 300, margin: 20 }}
-                  cover={
-                    <img
-                      alt=""
-                      src={photo_url}
-                      style={{
-                        width: 300,
-                        height: 300,
-                        objectFit: 'contain'
-                      }}
-                    />
-                  }
-                  actions={[<MessageOutlined key="message" />, <LikeOutlined key="like" />]}>
-                  <Meta title={item_name} description={category} />
-                </Card>
-              </Col>
-            ))}
-          </Row>
+            {listingsReturned.map(
+              ({ category, item_name, photo_url }, index) => (
+                <Col span={6} key={index}>
+                  <Card
+                    hoverable
+                    style={{ width: 300, margin: 20 }}
+                    cover={
+                      <img
+                        alt=""
+                        src={photo_url}
+                        style={{ width: 300, height: 300, objectFit: 'contain' }}
+                      />
+                    }
+                    actions={[<MessageOutlined key="message" />, <LikeOutlined key="like" />]}>
+                    <Meta title={item_name} description={category} />
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+
+            <Space wrap>
+              <Button
+                className="button"
+                type="primary"
+                style={{ backgroundColor: '#ff7e55', color: 'white' }}
+                onClick={loginWithRedirect}>
+                Sign Up/Login
+              </Button>
+            </Space>
+          </div>
+          <div id="about">
+            <h2>Join us with these 3 simple steps:</h2>
+            <img className="banner" src={banner} alt="banner" />
+          </div>
+          <div id="press" className="press">
+            <h2>We are supported by</h2>
+            <Carousel slidesToShow={3} style={{ margin: '20px 50px' }} autoplay>
+              <div className="slide">
+                <img src={logo} alt="rocket" />
+              </div>
+              <div className="slide">
+                <img src={intropage} alt="rocket" />
+              </div>
+              <div className="slide">
+                <img src={banner} alt="rocket" />
+              </div>
+              <div className="slide">
+                <img src={rocket} alt="rocket" />
+              </div>
+              <div className="slide">
+                <img src={intropage} alt="rocket" />
+              </div>
+              <div className="slide">
+                <img src={banner} alt="rocket" />
+              </div>
+            </Carousel>
+          </div>
+          <div className="contact" id="contact">
+            <h2>Contact us</h2>
+            <p>Have any questions? Get in touch with us!</p>
+            <div className="contactfields">
+              <div>email: hello@giveandtake.sg</div>
+            </div>
+          </div>
         </Content>
         <Footer style={footerStyle}>Copyright © Give & Take 2023 </Footer>
       </Layout>
