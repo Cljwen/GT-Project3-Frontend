17108364f1472fe24ab42417219cfcc92c3aabf9
longpaladin <frostarcher7@gmail.com>
2023-02-23 00:35:01 +0800

    solved auth0 loading page redirection and create profile page

--- src/pages/CreateProfile/index.js
+++ src/pages/CreateProfile/index.js
@@ -9,15 +9,24 @@ import {
   contentStyle,
   footerStyle,
   replicateFooterStyle
-} from '../globalstyles.js';
-import { Navbar } from '../../commoncomponents/Navbar/Navbar';
-import { PlusOutlined } from '@ant-design/icons';
-import { Button, Form, Input, TreeSelect, Upload, Select, Layout, notification } from 'antd';
-import { useAuth0 } from '@auth0/auth0-react';
-import { SmileOutlined, FrownOutlined } from '@ant-design/icons';
-import { UploadWidget } from './UploadWidget';
-import axios from 'axios';
-import { useParams, Link, Navigate, useNavigate } from 'react-router-dom';
+} from "../globalstyles.js";
+import { Navbar } from "../../commoncomponents/Navbar/Navbar";
+import { PlusOutlined } from "@ant-design/icons";
+import {
+  Button,
+  Form,
+  Input,
+  TreeSelect,
+  Upload,
+  Select,
+  Layout,
+  notification,
+} from "antd";
+import { useAuth0 } from "@auth0/auth0-react";
+import { SmileOutlined, FrownOutlined } from "@ant-design/icons";
+import { UploadWidget } from "./UploadWidget";
+import axios from "axios";
+import { useParams, Link, Navigate, useNavigate } from "react-router-dom";
 
 export function CreateProfile() {
   const { TextArea } = Input;
@@ -66,7 +75,6 @@ export function CreateProfile() {
       .catch(function (error) {
         console.log(error);
       });
-    
   };
   const openFailureNotification = (placement) => {
     api.info({
@@ -76,10 +84,10 @@ export function CreateProfile() {
       icon: (
         <FrownOutlined
           style={{
-            color: 'red'
+            color: "red",
           }}
         />
-      )
+      ),
     });
   };
 
@@ -94,11 +102,11 @@ export function CreateProfile() {
       .post(`http://localhost:3000/createuser`, formValues, configs)
       .then(function (response) {
         console.log(response);
-        openSuccessNotification('top');
+        openSuccessNotification("top");
       })
       .catch(function (error) {
         console.log(error);
-        openFailureNotification('top');
+        openFailureNotification("top");
       });
   };
 
@@ -109,7 +117,7 @@ export function CreateProfile() {
         last_name: user.family_name,
         first_name: user.given_name,
         username: user.nickname,
-        profile_photo: user.picture
+        profile_photo: user.picture,
       });
     }
   }, [user]);
@@ -127,88 +135,118 @@ export function CreateProfile() {
   return (
     <div>
       {contextHolder}
-      <br></br>
       <Layout>
         <Sider width={300} style={siderStyle}>
-          {/* <Navbar /> */}
+          <Navbar />
           <Footer style={replicateFooterStyle}> </Footer>
         </Sider>
         <Layout>
           <Content style={contentStyle}>
-            <div className="container">
-              <div className="content">
-                {user && (
-                  <Form
-                    labelCol={{
-                      span: 8
-                    }}
-                    wrapperCol={{
-                      span: 20
-                    }}
-                    layout="horizontal"
-                    style={{
-                      maxWidth: 800
-                    }}>
-                    <Form.Item label="Current Profile Photo" name="profile_photo">
-                      <img alt="" src={formValues.profile_photo} width="200" />
-                      <br></br>
-                      <UploadWidget setFormValues={setFormValues} formValues={formValues} />
-                    </Form.Item>
-                    <Form.Item
-                      label="Username"
-                      name="username"
-                      onChange={handleInputChange}
-                      defaultValue={user.nickname}>
-                      <Input
-                        placeholder={user.nickname}
-                        // defaultValue={user.nickname}
-                      />
-                    </Form.Item>
-                    <Form.Item label="First Name" name="first_name" onChange={handleInputChange}>
-                      <Input placeholder={formValues.first_name} />
-                    </Form.Item>
-                    <Form.Item label="Last Name" name="last_name" onChange={handleInputChange}>
-                      <Input placeholder={formValues.last_name} />
-                    </Form.Item>
-                    <Form.Item label="Email" name="email" onChange={handleInputChange}>
-                      <Input placeholder={formValues.email} />
-                    </Form.Item>
+            <div>
+              {user && (
+                <Form
+                  labelCol={{
+                    span: 8,
+                  }}
+                  wrapperCol={{
+                    span: 20,
+                  }}
+                  layout="horizontal"
+                  style={{
+                    maxWidth: 800,
+                  }}
+                >
+                  <Form.Item label="Current Profile Photo" name="profile_photo">
+                    <img alt="" src={formValues.profile_photo} width="200" />
+                    <br></br>
+                    <UploadWidget
+                      setFormValues={setFormValues}
+                      formValues={formValues}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    label="Username"
+                    name="username"
+                    onChange={handleInputChange}
+                    defaultValue={user.nickname}
+                  >
+                    <Input
+                      placeholder={user.nickname}
+                      // defaultValue={user.nickname}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    label="First Name"
+                    name="first_name"
+                    onChange={handleInputChange}
+                  >
+                    <Input placeholder={formValues.first_name} />
+                  </Form.Item>
+                  <Form.Item
+                    label="Last Name"
+                    name="last_name"
+                    onChange={handleInputChange}
+                  >
+                    <Input placeholder={formValues.last_name} />
+                  </Form.Item>
+                  <Form.Item
+                    label="Email"
+                    name="email"
+                    onChange={handleInputChange}
+                  >
+                    <Input placeholder={formValues.email} />
+                  </Form.Item>
 
-                    <Form.Item
-                      name="phone_number"
-                      label="Phone Number"
-                      rules={[
-                        {
-                          required: true,
-                          message: 'Please input your phone number!'
-                        }
-                      ]}
-                      onChange={handleInputChange}>
-                      <Input addonBefore="+65" placeholder={formValues.phone_number} />
-                    </Form.Item>
-                    <Form.Item label="Address" name="address" onChange={handleInputChange}>
-                      <Input placeholder={formValues.address} />
-                    </Form.Item>
-                    <Form.Item label="Postal Code" name="postal_code" onChange={handleInputChange}>
-                      <Input placeholder={formValues.postal_code} />
-                    </Form.Item>
-                    <Form.Item label="Nearest MRT" name="mrt" onChange={handleInputChange}>
-                      <Input placeholder={formValues.mrt} />
-                    </Form.Item>
+                  <Form.Item
+                    name="phone_number"
+                    label="Phone Number"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your phone number!",
+                      },
+                    ]}
+                    onChange={handleInputChange}
+                  >
+                    <Input
+                      addonBefore="+65"
+                      placeholder={formValues.phone_number}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    label="Address"
+                    name="address"
+                    onChange={handleInputChange}
+                  >
+                    <Input placeholder={formValues.address} />
+                  </Form.Item>
+                  <Form.Item
+                    label="Postal Code"
+                    name="postal_code"
+                    onChange={handleInputChange}
+                  >
+                    <Input placeholder={formValues.postal_code} />
+                  </Form.Item>
+                  <Form.Item
+                    label="Nearest MRT"
+                    name="mrt"
+                    onChange={handleInputChange}
+                  >
+                    <Input placeholder={formValues.mrt} />
+                  </Form.Item>
 
-                    <Form.Item>
-                      <Button
-                        style={{ backgroundColor: 'white', color: '#ff7e55', marginRight: '20px' }}
-                        type="primary"
-                        htmlType="submit"
-                        onClick={handleSubmit}>
-                        Save Changes
-                      </Button>
-                      <Link to={`/${user_id}/profile`}>Back to Profile</Link>
-                    </Form.Item>
-                  </Form>
-                )}
-              </div>
+                  <Form.Item>
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      onClick={handleSubmit}
+                    >
+                      Save Changes
+                    </Button>
+                    <Link to={`/${user_id}/profile`}>Back to Profile</Link>
+                  </Form.Item>
+                </Form>
+              )}
             </div>
           </Content>
           <Footer style={footerStyle}>Copyright G&T 2023</Footer>
