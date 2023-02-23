7917c66e183921af7fc0c7cf17382eddf85119b3
longpaladin <frostarcher7@gmail.com>
2023-02-24 03:28:01 +0800

    solved createprofile upload photo bug

--- src/pages/CreateProfile/index.js
+++ src/pages/CreateProfile/index.js
@@ -43,7 +43,7 @@ export function CreateProfile() {
     from: "frostarcher7@gmail.com", // Change to your verified sender
     subject: "Give&Take - Your account has been created!",
     text: `Hi ${formValues.username}, Thank you for creating an account with Give&Take! You may start putting your listings!`,
-    html: `<img alt="" width="300" src="https://res.cloudinary.com/dwgeik14h/image/upload/v1675348723/give_and_take_logo.png" /><br></br><h2>Hi ${formValues.username}!</h2><br></br><p>Thank you for creating an account with Give&Take! You may start putting your listings!</p><br></br><p>Login now by clicking <a href="http://localhost:3001">here</a>!</p><br></br><p>Regards,</p></br><p>Give & Take team</p>`,
+    html: `<img alt="" src="https://res.cloudinary.com/dwgeik14h/image/upload/v1675348723/give_and_take_logo.png" /><br></br><h2>Hi ${formValues.username}!</h2><br></br><p>Thank you for creating an account with Give&Take! You may start putting your listings!</p><br></br><p>Login now by clicking <a href="http://localhost:3001">here</a>!</p><br></br><p>Regards,</p></br><p>Give & Take team</p>`,
   };
   const openSuccessNotification = (placement) => {
     api.info({
@@ -75,6 +75,7 @@ export function CreateProfile() {
       .catch(function (error) {
         console.log(error);
       });
+    
   };
   const openFailureNotification = (placement) => {
     api.info({
@@ -135,9 +136,10 @@ export function CreateProfile() {
   return (
     <div>
       {contextHolder}
+      <br></br>
       <Layout>
         <Sider width={300} style={siderStyle}>
-          <Navbar />
+          {/* <Navbar /> */}
           <Footer style={replicateFooterStyle}> </Footer>
         </Sider>
         <Layout>
@@ -243,7 +245,7 @@ export function CreateProfile() {
                     >
                       Save Changes
                     </Button>
-                    <Link to={`/${user_id}/profile`}>Back to Profile</Link>
+                    
                   </Form.Item>
                 </Form>
               )}
